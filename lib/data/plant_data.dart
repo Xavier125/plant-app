84426bc5a2864e8d526cc6d4a4b938a9169500f4
Xavier <xavierooi99@gmail.com>
2024-12-02 14:44:38 +0800

    Add plant data

--- lib/data/plant_data.dart
+++ lib/data/plant_data.dart
@@ -5,21 +5,21 @@ List<Plant> plantData() {
     Plant(
       id: 1,
       name: 'Aloe Vera',
-      price: 19.99,
+      price: 19.60,
       description: 'Aloe Vera is a succulent plant species of the genus Aloe.',
       rating: 4,
     ),
     Plant(
       id: 2,
       name: 'Snake Plant',
-      price: 29.99,
+      price: 29.40,
       description: 'Snake plants are known for their air-purifying qualities.',
       rating: 3,
     ),
     Plant(
       id: 3,
       name: 'Spider Plant',
-      price: 15.99,
+      price: 15.50,
       description:
           'The spider plant is a popular indoor plant with green and white striped leaves.',
       rating: 4,
@@ -27,7 +27,7 @@ List<Plant> plantData() {
     Plant(
       id: 4,
       name: 'Ficus Tree',
-      price: 39.99,
+      price: 39,
       description:
           'A large tropical plant often grown as a houseplant for its attractive foliage.',
       rating: 5,
@@ -35,14 +35,14 @@ List<Plant> plantData() {
     Plant(
       id: 5,
       name: 'Cactus',
-      price: 9.99,
+      price: 9.90,
       description: 'Cacti are succulent plants known for their spiny stems.',
       rating: 2,
     ),
     Plant(
       id: 6,
       name: 'Peace Lily',
-      price: 24.99,
+      price: 24.90,
       description:
           'Peace lilies are known for their beautiful white flowers and air-purifying properties.',
       rating: 1,
@@ -50,7 +50,7 @@ List<Plant> plantData() {
     Plant(
       id: 7,
       name: 'Monstera',
-      price: 49.99,
+      price: 49.90,
       description:
           'Monstera is a large-leaved tropical plant known for its unique appearance.',
       rating: 2,
@@ -58,7 +58,7 @@ List<Plant> plantData() {
     Plant(
       id: 8,
       name: 'ZZ Plant',
-      price: 34.99,
+      price: 34.90,
       description:
           'ZZ plants are known for their tolerance to low light and neglect.',
       rating: 3,
@@ -66,7 +66,7 @@ List<Plant> plantData() {
     Plant(
       id: 9,
       name: 'Pothos',
-      price: 12.99,
+      price: 12.20,
       description:
           'Pothos is a fast-growing vine that is popular as an indoor plant.',
       rating: 1,
@@ -74,7 +74,7 @@ List<Plant> plantData() {
     Plant(
       id: 10,
       name: 'Succulent',
-      price: 19.99,
+      price: 19.50,
       description:
           'Succulents are water-retaining plants adapted to dry climates.',
     ),
